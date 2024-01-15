4af845e767444218b802bbc994de188b98382ab6
sutudyy <withoutmusiclife00@gmail.com>
2024-01-15 22:21:27 +0900

    コミット５

--- Java/design_pattern/work/git_test/src/git_test/test.java
+++ Java/design_pattern/work/git_test/src/git_test/test.java
@@ -4,5 +4,6 @@ public class test {
 	public static void main(String[] args) {
 		System.out.println("あああああああああああああああああ");
 		System.out.println("いいいいいいいいいいいいいいいいいい");
+		System.out.println("いいいいいいいいいいいいいいいいいい");
 	}
 }
